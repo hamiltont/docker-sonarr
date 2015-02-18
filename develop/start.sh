7ae212b11fb177748e18b37701ebc2e25a6bd385
Hamilton Turner <hamiltont@gmail.com>
2015-02-18 18:57:13 -0500

    Typos

--- develop/start.sh
+++ develop/start.sh
@@ -26,14 +26,14 @@ COMPLETED_ID=$(stat -c "%g" /volumes/completed)
 # group if needed
 GID_COUNTER=0
 function add_to_group {
-  ID = $1
+  ID=$(echo $1)
   EXISTS=$(cat /etc/group | grep $ID | wc -l)
   
   # If doesn't exist, create new group using that GID
   # and add nobody user 
   if [ $EXISTS == "0" ]; then 
-    groupadd -g $ID tempgroup-$GID_COUNTER
-    usermod -a -G tempgroup-$GID_COUNTER nobody
+    groupadd -g $ID tempgroup$GID_COUNTER
+    usermod -a -G tempgroup$GID_COUNTER nobody
     echo "Created group $(getent group $ID)"
     GID_COUNTER=$((GID_COUNTER + 1))
   else 
@@ -44,9 +44,9 @@ function add_to_group {
   fi
 }
 
-GIDS=$(printf "$MEDIA_ID\n$CONFIG_ID\n$COMPLETED_ID" | sort | uniq)
+GIDS=$(printf "$MEDIA_ID\n$CONFIG_ID\n$COMPLETED_ID\n" | sort | uniq)
 echo "Adding nobody to host GIDs: $(echo $GIDS | tr '\n' ',')"
-echo $GIDS | xargs add_to_group
+printf $GIDS | while read line ; do add_to_group $line ; done
 
 echo "starting sonarr as nobody"
 chsh -s /bin/sh nobody
